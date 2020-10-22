841a9c0de2df58631855a413569aa0e188c71e32
raferti <raferti712@yandex.ru>
2020-10-22 16:10:04 +0300

    get environ variables

--- django_journal/settings.py
+++ django_journal/settings.py
@@ -3,7 +3,7 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY', 'unsafe-secret-key')
 
 DEBUG = True
 
